d97c4234fb131d94d3f926b5f2fc6b00bb3a3b3d
dastan <dastan.kubanuulu1@gmail.com>
2022-01-30 19:52:54 +0600

    lol

--- djngogirl/blog/urls.py
+++ djngogirl/blog/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from .views import *
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', post_list, name='post_list'),
+    path('post/<int:pk>/', post_detail, name='post_detail'),
+    path('post/new/', post_new, name='post_new'),
+    path('post/<int:pk>/edit/', post_edit, name='post_edit'),
+
 ]
